4d6388753a72bc67bb13e9663c90cd9ae236dc46
Himanshu <himanshumishrais11@gmail.com>
2021-02-16 21:31:57 +0530

    16-02-2021

--- app/src/main/java/com/example/grewords/MainActivity.kt
+++ app/src/main/java/com/example/grewords/MainActivity.kt
@@ -43,16 +43,30 @@ class MainActivity : AppCompatActivity() {
         val userEmailid=findViewById<EditText>(R.id.user_email_Id)
         val userPassword=findViewById<EditText>(R.id.user_password)
         val userPhonenumber=findViewById<EditText>(R.id.user_phonenumber) // to use.
-
+        var validatepassword=1
+        var uppercase_count=0
+        var lowercase_count=0
+        var specialchr_count=0
+        for(c in userPassword.text.toString())
+        {
+            if(c>='a'&&c<='z')
+                lowercase_count++
+            else if(c>='A'&&c<='Z')
+                uppercase_count++
+            else
+                specialchr_count++
+        }
+        if(lowercase_count==0||uppercase_count==0||specialchr_count==0)
+            validatepassword=0
         if(userEmailid.text.toString().isEmpty())
         { // In case the user emailid entered is empty
             userEmailid.error="Please enter a valid Email"
             userEmailid.requestFocus()
             return
         }
-        if(userPassword.text.toString().length<6) // password strength
+        if(userPassword.text.toString().length<6||validatepassword==0) // password strength
         {
-            userPassword.error="Please enter a strong password with more than 6 characters"
+            userPassword.error="Please enter a strong password with more than 6 characters with atleast one uppercase, on lowercase and one special character"
             userPassword.requestFocus()
             return
 
