210187029674db2ad8faaa79e00ba806a1ca8bf9
devilmonk <rishabhjain08769@gmail.com>
2021-02-28 00:31:37 +0530

    stack0 created

--- app/src/main/java/com/example/grewords/MainActivity.kt
+++ app/src/main/java/com/example/grewords/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.grewords
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.data.model.PhoneNumber
+import com.firebase.ui.auth.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
-     private var firebaseAuth=FirebaseAuth.getInstance()
+    private var firebaseAuth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,17 +38,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun login(view:View)
+    fun login(view: View)
     { // Login activity go
-        startActivity(Intent(this,LoginActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
-    fun register(view:View)
+    fun register(view: View)
     {
 
         val userEmailid=findViewById<EditText>(R.id.user_email_Id)
         val userPassword=findViewById<EditText>(R.id.user_password)
-        val userPhonenumber=findViewById<EditText>(R.id.user_phonenumber) // to use.
+        val phoneNumber=findViewById<EditText>(R.id.user_phonenumber).toString() // to use.
         var validatepassword=1
         var uppercase_count=0
         var lowercase_count=0
@@ -71,17 +77,18 @@ class MainActivity : AppCompatActivity() {
             return
 
         }
-        firebaseAuth.createUserWithEmailAndPassword(userEmailid.text.toString(),userPassword.text.toString())
-            .addOnCompleteListener(this){task->
+        firebaseAuth.createUserWithEmailAndPassword(userEmailid.text.toString(), userPassword.text.toString())
+            .addOnCompleteListener(this){ task->
                 if(task.isSuccessful)
                 {
                     val currentUser=firebaseAuth.currentUser
                     currentUser?.sendEmailVerification() //send user verification email
-                        ?.addOnCompleteListener{task1 ->
+                        ?.addOnCompleteListener{ task1 ->
                             if(task1.isSuccessful)
                             {
                                 // Login activity go
-                                startActivity(Intent(this,LoginActivity::class.java))
+                                createList()
+                                startActivity(Intent(this, LoginActivity::class.java))
                                 finish()
                             }
                             
@@ -90,9 +97,30 @@ class MainActivity : AppCompatActivity() {
                 }
                 else
                 {
-                    Toast.makeText(this,"Try again after some time",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Try again after some time", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    private fun createList(){
+        val scan = Scanner(
+                resources.openRawResource(R.raw.grewords))
+        readFileHelper(scan)
+    }
+    private fun readFileHelper(scan: Scanner){
+        while(scan.hasNextLine()){
+            val line: String = scan.nextLine()
+            val parts = line.split(":")
+            if(parts.size<2) continue
+
+            if(FirebaseAuth.getInstance().currentUser == null){
+                Log.d("debug", "what the hell")
+            }else if(FirebaseAuth.getInstance().currentUser !=null){
+                var root = FirebaseDatabase.getInstance().getReference()
+                root.child("users").child(FirebaseAuth.getInstance().currentUser!!.uid)
+                        .child("stack0").child(parts[0]).setValue(parts[1])
+            }
+        }
     }
 
 }
